4a1cddf3596997628197d648a6995d6e030a331d
rahachu <muhammadrahadian86@gmail.com>
2019-09-14 17:11:58 +0700

    Membuat konstruktor dan penulis matriks

--- matriks.java
+++ matriks.java
@@ -1,5 +1,33 @@
+import java.util.*;
+
 public class matriks {
+    Scanner scan = new Scanner(System.in);
+    private int baris;
+    private int kolom;
+    private int[][] matriks;
 
+    public matriks() {
+        System.out.print("Masukan jumlah baris:");
+        baris = scan.nextInt();
+        System.out.print("Masukan jumlah kolom:");
+        kolom = scan.nextInt();
+        matriks = new int [baris][kolom];
+        for (int i = 0; i < baris; i++) {
+            for (int j = 0; j < kolom; j++) {
+                matriks[i][j]= scan.nextInt();
+            }
+        }
+    }
 
+    public void tulis() {
+        for (int i = 0; i < baris; i++) {
+            for (int j = 0; j < kolom; j++) {
+                System.out.print(matriks[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+
+    
 
 }
