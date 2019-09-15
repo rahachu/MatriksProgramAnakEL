3707c65b7fff4aca096e9cd6d2eb0fedb92f35d7
rahachu <muhammadrahadian86@gmail.com>
2019-09-15 14:40:29 +0700

    adding obe procedure

--- matriks.java
+++ matriks.java
@@ -4,7 +4,7 @@ public class matriks {
     Scanner scan = new Scanner(System.in);
     private int baris;
     private int kolom;
-    private int[][] matriks;
+    private double[][] matriks;
 
     //Konstruktor matriks
     public matriks() {
@@ -46,6 +46,35 @@ public class matriks {
         matriks[brs][klm]=scan.nextInt();
     }
 
-    
+    //Operasi Baris Elementer
+    public void tambahBaris(int kali,int baris1, int baris2) {
+        for (int i = 0; i < kolom; i++) {
+            matriks[baris1][i] += matriks[baris2][i]*kali;
+        }
+    }
+
+    public void bagiBaris(int baris1,int x) {
+        for (int i = 0; i < kolom; i++) {
+            matriks[baris1][i] /= x;
+        }
+    }
+
+    public void buat1Utama(int baris1) {
+        
+    }
+
+    //Gauss
+
+    //Gauss Jordan
+    public void gaussJordan() {
+        gauss();
+        for (int i = kolom-2; i >= 0; i--) {
+            for (int j = baris-1; j >= 0; j++) {
+                if (getELmt(j, i)!=0) {
+                    obe;
+                }
+            }
+        }
+    }
 
 }
