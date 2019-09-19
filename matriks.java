ca5503572169cf7274127b1baf4c957922c5b17d
rahachu <muhammadrahadian86@gmail.com>
2019-09-20 03:16:05 +0700

    matriks

--- matriks.java
+++ matriks.java
@@ -23,8 +23,8 @@ public class matriks {
         return (matriks[brs][klm]);
     }
     public double [] getElmtBaris(int brs){
+        double [] temp = new double[kolom];
         for (int i = 0; i < baris; i++){
-                double [] temp = new double[kolom];
                 temp[i] = getELmt(brs, i);
             }
     
@@ -62,11 +62,8 @@ public class matriks {
         }
     }
 
-<<<<<<< HEAD
-    public void setElmt(int brs ,int klm, double elmt) {
-=======
+
     public void setElmt(int brs ,int klm, Double elmt) {
->>>>>>> 95c004e725cda211f1a73c25269d606f51867e24
         matriks[brs][klm]=elmt;
     }
     public void setBaris(int brs, double[] el){
--- point.java
+++ point.java
@@ -1,14 +1,37 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class point {
     Scanner scan = new Scanner(System.in);
-    int x;
-    int y;
 
-    public point() {
-        System.out.print("Masukan nilai x:");
-        x = scan.nextInt();
-        System.out.print("Masukan nilai y:");
-        y = scan.nextInt();
+    public void point() {
+        int a = scan.nextInt();
+        System.out.print("Masukan nilai x y: ");
+        double[][] temp = new double[a][2];
+        for(int i = 0; i<a; i++){
+            for(int j = 0; j<2; j++){
+            temp[i][j] = scan.nextInt();
+            }
+        }
+    }
+    public void pointToMatriks(matriks a){
+        int x;
+        matriks save = new matriks();
+        for(int i = 0; i< a.getBaris(); i++){
+            for(int j = 0; j<2; j++){
+            double tempx = a.getELmt(i, j);
+            
+                for(int k = 0; k<=a.getBaris(); k++){
+                    if(k<a.getBaris() && j ==0 ){
+                    save.setElmt(i, k, Math.pow(tempx,k));
+                    }
+                    else{
+                        save.setElmt(i,a.getKolom(),tempx);
+                    }
+                }
+            }
+        }
+        
+
+        
     }
 }
